37b2f4b96a7a29778894f4438787dd73702211b2
tyagapn02 <93714708+silvershark0202@users.noreply.github.com>
2021-11-04 20:53:55 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,16 +1,16 @@
-import sys #buat ngetik cmd commands
+import sys
 import socket
 from datetime import datetime
 
-#targetnya
+
 if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1]) #ganti host name --> IPv4
+    target = socket.gethostbyname(sys.argv[1])
 else:
     print("Argumen salah.")
     print("syntax: python3 scanner.py <ip>")
     sys.exit()
     
-#banner biar bagus
+
 print("-" * 50)
 print(f"scanning target {target}")
 print("time : " + str(datetime.now()))
@@ -27,18 +27,13 @@ try:
         s.close()
         
 except KeyboardInterrupt:
-    print("\nKeluar. ")
+    print("\nexit. ")
     sys.exit()
     
 except socket.gaierror:
-    print("Hostname could not be resolved.")
+    print("Hostname error.")
     sys.exit()
     
 except socket.error:
-    print("Couldn't connect to server.")
+    print("Cannot connect to server")
     sys.exit()
-
-#scanner.py by tyagapn02
-#on THM
-#MD5 = 02d5b23d19c8ca94a761fcb21e37408c
-#for bro J
